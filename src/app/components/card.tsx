ec117034898cd83bef573bc58bf047088f61cc99
shayan memarzade <shayan.memarzade@live.com>
2024-12-11 20:47:05 +0300

    feat: add status icon

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -6,6 +6,17 @@ type Props = {
 }
 
 export default function Card({ character }: Props) {
+
+  const statusColor = (status: string) => {
+    if (status === 'Dead') {
+      return 'bg-red-600'
+    }
+    if (status === 'Alive') {
+      return 'bg-green-600'
+    }
+    return 'bg-gray-400'
+  }
+
   return (
     <article className="flex flex-col md:flex-row text-white rounded-lg overflow-hidden">
       <div className="max-h-60 md:w-5/12">
@@ -16,14 +27,17 @@ export default function Card({ character }: Props) {
           className='w-full h-full object-cover' />
       </div>
       <div className="bg-slate-600 p-4 md:w-7/12">
-        <div className="section">
-          <a href={character.url} rel="noopener noreferrer" target="_blank" className="font-bold text-xl">
+        <div className="section mb-3">
+          <a href={character.url} rel="noopener noreferrer" target="_blank" className="font-bold text-2xl">
             <h2>{character.name}</h2>
           </a>
-          <span className="status"><span className="status__icon"></span> {character.status} - {character.species}</span>
+          <span className="gap-2 flex items-center">
+            <div className={`w-2 h-2 rounded-full ${statusColor(character.status)}`}></div>
+            {character.status} - {character.species}
+          </span>
         </div>
         <div className="section">
-          <span className="text-gray-400 block">Last known location:</span>
+          <span className="text-gray-400 block text-sm">Last known location:</span>
           <a href={character.location.url} rel="noopener noreferrer" target="_blank" className=""> {character.location.name}</a>
         </div>
       </div>
